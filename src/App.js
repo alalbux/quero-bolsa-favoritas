988c2ab2d91f7c34a054510645b24fa462a7efd9
Amanda Albuquerque <amandlei@hotmail.com>
2019-08-26 20:25:17 -0300

    feat(quero-bolsa): add Button component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './App.css'
+import Button from './components/Button'
 
 function App() {
   return (
     <div className="App">
       Home
+      <Button> Test </Button>
     </div>
   )
 }
