49d0b145b24d893d574b24c2f619dea4aa2d8b6c
Amanda Albuquerque <amandlei@hotmail.com>
2019-08-26 22:28:05 -0300

    feat(quero-bolsa): add Menu component

---
+++ src/components/Menu/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Menu.css'
+
+const Menu = () => (
+  <div
+    className='Menu'
+  >
+    <ul>
+      <li>Minha conta</li>
+      <li>Pré-matrículas</li>
+      <li>Bolsas favoritas</li>
+    </ul>
+  </div>
+)
+
+export default Menu
