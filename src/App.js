dc02d39197c7bfe8f9579e12cff52a0d29d2d690
Amanda Albuquerque <amandlei@hotmail.com>
2019-08-27 20:06:00 -0300

    feat(quero-bolsa): add Select and Breadcrumbs components in App

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import {
   Header,
   Menu,
   //test
+  Breadcrumbs,
   Card,
   Link,
   Text,
   Checkbox,
   Input,
+  Select,
   Flexbox,
   Modal,
   Page,
@@ -27,28 +29,34 @@ class App extends React.Component {
       <Page>
         <Grid>
           <Cell size={[12, 12, 12]}>
-            <Header height='100px'/>
+            <Header height='100px' />
             <Menu />
-            Home
+            <Page>test page
             <Button> Test </Button>
-            <Card>Test</Card>
-            <Link>Test</Link>
-            <Text>Test</Text>
-            <Checkbox>Test</Checkbox>
-            <Flexbox>
-              <div>teste flex</div>
-              <div>teste flex</div>
-              <div>teste flex</div>
-              <div>teste flex</div>
-            </Flexbox>
-            <Flexbox>
-              <button onClick={() => this.setState({ opened: true })}>Abrir modal</button>
-              <Modal opened={this.state.opened} onClose={() => this.setState({ opened: false })}>
-                <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam dapibus lacinia ultricies. Aliquam eu porttitor eros. Integer vitae porttitor ipsum. Curabitur ut ipsum in velit ornare feugiat. Phasellus non condimentum ex. Proin hendrerit faucibus dictum. Quisque condimentum tincidunt tortor, nec sodales nisi vulputate in. Donec pellentesque, leo in viverra blandit, felis risus tincidunt arcu, vel aliquam massa quam in enim. Phasellus vitae convallis erat. In cursus mi et urna ullamcorper imperdiet. Morbi pellentesque, dolor in aliquam placerat, mi lacus accumsan nibh, vel ultrices mi diam non nisi. Proin eros lacus, lobortis sed elementum at, efficitur vitae ex. Vivamus quam lacus, imperdiet vitae auctor a, varius nec sem. Nullam tincidunt, neque et interdum hendrerit, metus est hendrerit urna, quis consequat sem nibh ac orci. Proin dapibus hendrerit mauris, sed tincidunt urna rutrum et.</Text>
-              </Modal>
-            </Flexbox>
-            <Page>test page</Page>
-            <Input placeholder='Placeholder' />
+              <Card>Test</Card>
+              <Link>Test</Link>
+              <Text>Test</Text>
+              <Breadcrumbs />
+              <Checkbox>Test</Checkbox>
+              <Flexbox>
+                <div>teste flex</div>
+                <div>teste flex</div>
+                <div>teste flex</div>
+                <div>teste flex</div>
+              </Flexbox>
+              <Flexbox>
+                <button onClick={() => this.setState({ opened: true })}>Abrir modal</button>
+                <Modal opened={this.state.opened} onClose={() => this.setState({ opened: false })}>
+                  <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam dapibus lacinia ultricies. Aliquam eu porttitor eros. Integer vitae porttitor ipsum. Curabitur ut ipsum in velit ornare feugiat. Phasellus non condimentum ex. Proin hendrerit faucibus dictum. Quisque condimentum tincidunt tortor, nec sodales nisi vulputate in. Donec pellentesque, leo in viverra blandit, felis risus tincidunt arcu, vel aliquam massa quam in enim. Phasellus vitae convallis erat. In cursus mi et urna ullamcorper imperdiet. Morbi pellentesque, dolor in aliquam placerat, mi lacus accumsan nibh, vel ultrices mi diam non nisi. Proin eros lacus, lobortis sed elementum at, efficitur vitae ex. Vivamus quam lacus, imperdiet vitae auctor a, varius nec sem. Nullam tincidunt, neque et interdum hendrerit, metus est hendrerit urna, quis consequat sem nibh ac orci. Proin dapibus hendrerit mauris, sed tincidunt urna rutrum et.</Text>
+                </Modal>
+              </Flexbox>
+              <Select name='select-default'>
+                <option value='empty'>Select</option>
+                <option value='opt1'>First</option>
+                <option value='opt2'>Second</option>
+              </Select>
+              <Input placeholder='Placeholder' />
+            </Page>
           </Cell>
         </Grid>
       </Page>
