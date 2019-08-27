be13965212fdb2b59f080bf5e1e723a3dc5da48e
Amanda Albuquerque <amandlei@hotmail.com>
2019-08-27 16:46:44 -0300

    chore(quero-bolsa): refactor components export and remove title component

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,20 @@
-export { default as Button } from './content/Button'
+// cards
 export { default as Card } from './cards/Card'
+
+//content
+export { default as Button } from './content/Button'
+export { default as Link } from './content/Link'
+export { default as Text } from './content/Text'
+
+// form
+export { default as Checkbox } from './form/Checkbox'
+export { default as Input } from './form/Input'
+
+// layout
+export { default as Flexbox } from './layout/Flexbox'
 export { default as Header } from './layout/Header'
+export { default as Modal } from './layout/Modal'
+export { default as Page } from './layout/Page'
+
+// navigation
 export { default as Menu } from './navigation/Menu'
