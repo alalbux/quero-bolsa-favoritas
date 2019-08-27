7b4f1511e8a36c18650ecbb3a9040ceb0dbcacaf
Amanda Albuquerque <amandlei@hotmail.com>
2019-08-27 18:36:49 -0300

    feat(quero-bolsa): refactor Header component

--- src/components/layout/Page/index.js
+++ src/components/layout/Page/index.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import  { oneOfType, node, func } from 'prop-types'
+import { oneOfType, node, func } from 'prop-types'
 import styled from 'styled-components'
 
 const PageContainer = styled.div`
+  background-color: #FBFBFB;
   padding-bottom: 32px;
   width: 100%;
   margin: 0 auto;
@@ -16,7 +17,7 @@ const PageContent = styled.div`
 const Page = ({ children, height }) => (
   <PageContainer height={height}>
     <PageContent>
-    {children}
+      {children}
     </PageContent>
   </PageContainer>
 )
