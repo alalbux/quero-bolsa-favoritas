608264f3feee54769c3e95856931192ac7ddc5a2
Amanda Albuquerque <amandlei@hotmail.com>
2019-08-31 22:01:33 -0300

    feat(quero-bolsa): add Scholarships component

---
+++ src/components/list/Scholarships/index.js
@@ -0,0 +1,18 @@
+import { oneOfType, node, arrayOf } from 'prop-types'
+import styled from 'styled-components'
+
+const Scholarships = styled.ul`
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  display: table;
+  width: 100%;
+`
+
+Scholarships.propTypes = {
+  children: oneOfType([node, arrayOf(node)]).isRequired
+}
+
+Scholarships.displayName = 'Scholarships'
+
+export default Scholarships
