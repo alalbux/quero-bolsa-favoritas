d2fbc0a309948942b335997d7e37d171185f535e
Amanda Albuquerque <amandlei@hotmail.com>
2019-09-01 12:12:35 -0300

    feat(quero-bolsa): refactor ScholarshipsItem component

--- src/components/list/ScholarshipsItem/index.js
+++ src/components/list/ScholarshipsItem/index.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
 import { oneOfType, string, object, number } from 'prop-types'
 import styled from 'styled-components'
+import Checkbox from '../../form/Checkbox'
+import Text from '../../content/Text'
 
 const ScholarshipsText = styled.div`
-  display: table-cell;
   padding: 23px 35px 23px 40px;
   border-bottom: 1px solid #eeeeee;
   font-size: 16px;
@@ -11,18 +12,50 @@ const ScholarshipsText = styled.div`
   color: #333333;
 `
 
-const ScholarshipsItem = styled(class extends Component {
-  render () {
-    const { className, data } = this.props
+const ScholarshipsWrapper = styled.li`
+  display: flex;
+  justify-content: start;
+  align-content: center;
+`
 
+const ScholarshipsInfos = styled.div`
+  width: 100%;
+`
+
+const ScholarshipsLogo = styled.div`
+  min-width: 230px;
+
+  img {
+    max-width: 170px;
+    max-height: 90px;
+    padding: 16px 8px;
+    margin: 0 auto;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+`
+
+const ScholarshipsItem = styled(class extends Component {
+  render() {
+    const { university, course, discount_percentage, price_with_discount } = this.props
     return (
-      <li className={className}>
-        <ScholarshipsText>{data}</ScholarshipsText>
-      </li>
+      <ScholarshipsWrapper>
+        <ScholarshipsLogo>
+          <Checkbox><img src={university.logo_url} alt={university.name} /></Checkbox>
+        </ScholarshipsLogo>
+        <ScholarshipsInfos>
+          <Text>{course.name}</Text>
+          <Text>{course.level}</Text>
+        </ScholarshipsInfos>
+        <ScholarshipsText>Bolsa de {discount_percentage} {price_with_discount}</ScholarshipsText>
+      </ScholarshipsWrapper>
     )
   }
 })`
-  display: table-row;
+div {
+  width: 100%;
+}
 `
 
 ScholarshipsItem.propTypes = {
