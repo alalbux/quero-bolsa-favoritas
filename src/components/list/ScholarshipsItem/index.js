d288cc83e02b6d681e2f8694b3f6b4b7446bcf90
Amanda Albuquerque <amandlei@hotmail.com>
2019-08-31 22:02:29 -0300

    feat(quero-bolsa): add ScholarshipsItem component

---
+++ src/components/list/ScholarshipsItem/index.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+import { oneOfType, string, object, number } from 'prop-types'
+import styled from 'styled-components'
+
+const ScholarshipsText = styled.div`
+  display: table-cell;
+  padding: 23px 35px 23px 40px;
+  border-bottom: 1px solid #eeeeee;
+  font-size: 16px;
+  line-height: 1.35;
+  color: #333333;
+`
+
+const ScholarshipsItem = styled(class extends Component {
+  constructor (props) {
+    super(props)
+  }
+
+  render () {
+    const { className, data } = this.props
+
+    return (
+      <li className={className}>
+        <ScholarshipsText>{data}</ScholarshipsText>
+      </li>
+    )
+  }
+})`
+  display: table-row;
+`
+
+ScholarshipsItem.propTypes = {
+  date: oneOfType([string, object, number]).isRequired,
+  text: string.isRequired
+}
+
+ScholarshipsItem.displayName = 'ScholarshipsItem'
+
+export default ScholarshipsItem
