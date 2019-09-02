106cacd0e2060f7a484c42c2684aebeb3f1afe82
Amanda Albuquerque <amandlei@hotmail.com>
2019-09-02 16:39:26 -0300

    feat(quero-bolsa): refactor Breadcrumbs component

--- src/components/navigation/Breadcrumbs/index.js
+++ src/components/navigation/Breadcrumbs/index.js
@@ -1,19 +1,36 @@
+import React from 'react'
 import styled from 'styled-components'
-import Text from '../../content/Text'
+import Link from '../../content/Link'
 
-const Breadcrumbs = styled(Text)`
-  color: #ee0099;
-  text-decoration: none;
-
-  &:visited {
-    color: #b4006c;
-    text-decoration: none;
-  }
-
-  &:hover {
-    text-decoration: underline;
-  }
+const BreadcrumbsContainer = styled.div`
+  color: #007A8D;
+  padding: 24px 0;
+  font-size: 14px;
 `
+
+const BreadcrumbsText = styled.span`
+  color: #1F2D30;
+`
+const Bar = styled.span`
+  color: #1F2D30;
+  padding: 8px;
+`
+
+const LocalLink = styled(Link)`
+  color: #007A8D;
+  font-weight: bold;
+`
+
+const Breadcrumbs = () => (
+  <BreadcrumbsContainer>
+    <LocalLink>Minha conta</LocalLink>
+    <Bar>/</Bar>
+    <LocalLink>Pré-matrículas</LocalLink>
+    <Bar>/</Bar>
+    <BreadcrumbsText>Bolsas favoritas</BreadcrumbsText>
+  </BreadcrumbsContainer>
+)
+
 
 Breadcrumbs.displayName = 'Breadcrumbs'
 
