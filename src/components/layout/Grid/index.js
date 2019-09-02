c664f6bccb9897f1a8f77f7275ed50874095ddf2
Amanda Albuquerque <amandlei@hotmail.com>
2019-09-02 14:55:36 -0300

    feat(quero-bolsa): refactor Grid component

--- src/components/layout/Grid/index.js
+++ src/components/layout/Grid/index.js
@@ -1,12 +1,27 @@
-import styled from 'styled-components'
+import PropTypes from 'prop-types'
+import styled, { css } from 'styled-components'
+
+import config, { DIMENSION_NAMES } from '../../config'
 
 const Grid = styled.div`
-  width: 100%;
-  display: grid;
-  grid-template-columns: repeat(12, minmax(0, 1fr));
-  grid-gap: 30px;
+  margin-right: auto;
+  margin-left: auto;
+  padding-right: ${props => config(props).outerMargin + 'rem'};
+  padding-left: ${props => config(props).outerMargin + 'rem'};
+  ${props => !props.fluid && css`
+    ${DIMENSION_NAMES.map(t =>
+    config(props).container[t] && config(props).media[t]`
+        width: ${props => config(props).container[t]}rem;
+      `
+  )}
+  `}
 `
 
 Grid.displayName = 'Grid'
+
+Grid.propTypes = {
+  fluid: PropTypes.bool,
+  children: PropTypes.node
+}
 
 export default Grid
