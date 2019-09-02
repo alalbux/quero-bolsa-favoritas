40d419d9b8628b2e4ed80fd565dbd1ad943e58d8
Amanda Albuquerque <amandlei@hotmail.com>
2019-09-02 15:50:57 -0300

    feat(quero-bolsa): refactor Grid and Col component

--- src/App.js
+++ src/App.js
@@ -14,12 +14,12 @@ import {
   Modal,
   Page,
   Grid,
+  Row,
   Col,
   ButtonGroup,
   ScholarshipsCard,
   Scholarships,
-  ScholarshipsItem,
-  Container
+  ScholarshipsItem
 } from './components'
 
 class App extends Component {
@@ -60,11 +60,15 @@ class App extends Component {
           <Header height='100px' />
           <Menu />
           <Grid>
-            <Col size={[12, 12, 12]}>
-              <Container>
+            <Row>
+              <Col xs={12} sm={12} md={12} lg={12}>
                 <Breadcrumbs />
                 <Title.H1>Bolsas favoritas</Title.H1>
                 <Title.H2>Adicione bolsas de cursos e faculdades do seu interesse e receba atualização das melhores ofertas disponíveis</Title.H2>
+              </Col>
+            </Row>
+            <Row>
+              <Col xs={12} sm={12} md={12} lg={12}>
                 <Flexbox end>
                   <ButtonGroup
                     name='period-filter'
@@ -84,83 +88,80 @@ class App extends Component {
                     ]}
                   />
                 </Flexbox>
-              </Container>
-            </Col>
-            <Col size={[3, 3, 3]}>
-              <Card onClick={() => this.setState({ opened: true })}>
-                <Text>Adicionar bolsa</Text>
-                <Text>Clique para adicionar bolsas do seu interesse</Text>
-              </Card>
-            </Col>
-            <Container>
-              <Flexbox horizontal
-                colSize={12}>
-                {!isLoading ? (
-                  data.map(item => {
-                    const {
-                      university,
-                      course,
-                      price_with_discount,
-                      full_price,
-                      enabled
-                    } = item
-                    return (
-                      <Col size={[3, 3, 3]}>
-                        <ScholarshipsCard
-                          isEnabled={enabled}
-                          university={university}
-                          course={course}
-                          priceWithDiscount={price_with_discount}
-                          fullPrice={full_price}
-                        />
-                      </Col>
-                    )
-                  })
-                ) : (
-                    <h3>Loading...</h3>
-                  )}
-              </Flexbox>
-            </Container>
-            <Modal opened={this.state.opened}
-              onClose={() => this.setState({ opened: false })}
-              width="70"
-            >
-              <Title.H2>Adicionar bolsa</Title.H2>
-              <Text>Selecione sua cidade</Text>
-              <Select></Select>
-              <Text>Selecione seu curso de preferência</Text>
-              <Select></Select>
-              <Text>Como você quer estudar</Text>
-              <div>
-                <Checkbox>Presencial</Checkbox>
-                <Checkbox>A distância</Checkbox>
-              </div>
-              <Text>Até quanto pode pagar?</Text>
-
-              <div>
-                <Text>Resultado:</Text>
-                <Text>Ordenar por {this.state.filters}</Text>
+              </Col>
+            </Row>
+            <Row>
+              <Col xs={3} sm={3} md={3} lg={3}>
+                <Card onClick={() => this.setState({ opened: true })}>
+                  <Text>Adicionar bolsa</Text>
+                  <Text>Clique para adicionar bolsas do seu interesse</Text>
+                </Card>
+              </Col>
+              {!isLoading ? (
+                data.map(item => {
+                  const {
+                    university,
+                    course,
+                    price_with_discount,
+                    full_price,
+                    enabled
+                  } = item
+                  return (
+                    <Col xs={3} sm={3} md={3} lg={3}>
+                      <ScholarshipsCard
+                        isEnabled={enabled}
+                        university={university}
+                        course={course}
+                        priceWithDiscount={price_with_discount}
+                        fullPrice={full_price}
+                      />
+                    </Col>
+                  )
+                })
+              ) : (
+                  <h3>Loading...</h3>
+                )}
+              <Modal opened={this.state.opened}
+                onClose={() => this.setState({ opened: false })}
+                width="70"
+              >
+                <Title.H2>Adicionar bolsa</Title.H2>
+                <Text>Selecione sua cidade</Text>
+                <Select></Select>
+                <Text>Selecione seu curso de preferência</Text>
+                <Select></Select>
+                <Text>Como você quer estudar</Text>
                 <div>
-                  {error ? <p>{error.message}</p> : null}
-                  {!isLoading ? (
-                    <Scholarships>
-                      {data.map(item => {
-                        return (
-                          <ScholarshipsItem key={Math.random()} {...item} />
-                        )
-                      })}
-                    </Scholarships>
-                  ) : (
-                      <h3>Loading...</h3>
-                    )}
+                  <Checkbox>Presencial</Checkbox>
+                  <Checkbox>A distância</Checkbox>
                 </div>
-                <div>
-                  <Button>Cancelar</Button>
-                  <Button>Adicionar bolsa(s)</Button>
-                </div>
-              </div>
+                <Text>Até quanto pode pagar?</Text>
 
-            </Modal>
+                <div>
+                  <Text>Resultado:</Text>
+                  <Text>Ordenar por {this.state.filters}</Text>
+                  <div>
+                    {error ? <p>{error.message}</p> : null}
+                    {!isLoading ? (
+                      <Scholarships>
+                        {data.map(item => {
+                          return (
+                            <ScholarshipsItem key={Math.random()} {...item} />
+                          )
+                        })}
+                      </Scholarships>
+                    ) : (
+                        <h3>Loading...</h3>
+                      )}
+                  </div>
+                  <div>
+                    <Button>Cancelar</Button>
+                    <Button>Adicionar bolsa(s)</Button>
+                  </div>
+                </div>
+
+              </Modal>
+            </Row>
           </Grid>
           <Footer />
         </Flexbox>
