de9c68292cbbaeb6f4a2e5550c6f2bad2b4a031b
Amanda Albuquerque <amandlei@hotmail.com>
2019-09-02 15:49:25 -0300

    feat(quero-bolsa): refactor Menu component

--- src/components/navigation/Menu/index.js
+++ src/components/navigation/Menu/index.js
@@ -1,16 +1,64 @@
 import React from 'react'
-import './Menu.css'
+import styled from 'styled-components'
+
+import Link from '../../content/Link'
+import Grid from '../../layout/Grid'
+import Col from '../../layout/Col'
+import Row from '../../layout/Row'
+
+const MenuContainer = styled.div`
+  height: 60px;
+  background: #18ACC4;
+  padding: 16px 0;
+`
+
+const MenuList = styled.ul`
+  list-style: none;
+  display: flex;
+  flex-direction: row;
+  color: #ffffff;
+  font-size: 16px;
+  font-weight: 600;
+`
+
+const MenuItem = styled.li`
+  margin-right: 24px;
+`
+
+const MenuLink = styled(Link)`
+  color: #ffffff;
+  font-weight: bold;
+  font-size: 18px;
+  line-height: 1.5;
+`
+
+const MyAccount = styled(MenuLink)`
+  font-size: 20px;
+  line-height: 1.2;
+`
 
 const Menu = () => (
-  <div
-    className='Menu'
-  >
-    <ul>
-      <li>Minha conta</li>
-      <li>Pré-matrículas</li>
-      <li>Bolsas favoritas</li>
-    </ul>
-  </div>
+  <MenuContainer>
+    <Grid>
+      <Row>
+        <Col xs={12} sm={3} md={3} lg={3}>
+          <MenuList>
+            <MenuItem>
+              <MyAccount>Minha conta</MyAccount>
+            </MenuItem>
+            <MenuItem>
+              <MenuLink>Pré-matrículas</MenuLink>
+            </MenuItem>
+            <MenuItem>
+              <MenuLink>Bolsas favoritas</MenuLink>
+            </MenuItem>
+          </MenuList>
+        </Col>
+      </Row>
+    </Grid>
+  </MenuContainer>
 )
+
+Menu.displayName = 'Menu'
 
 export default Menu
