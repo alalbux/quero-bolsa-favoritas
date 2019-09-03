261af8b307cdf86c08991f9d46efaf04f875d7bc
Amanda Albuquerque <amandlei@hotmail.com>
2019-09-02 21:31:08 -0300

    feat(quero-bolsa): refactor Card component

--- src/components/cards/Card/index.js
+++ src/components/cards/Card/index.js
@@ -2,12 +2,16 @@ import styled from 'styled-components'
 
 const Card = styled.div`
   position: relative;
-  border-radius: ${props => props.rounded || '1px'};
+  border-radius: ${props => props.rounded || '3px'};
   background-color: #ffffff;
   box-shadow: 2px 1px 14px 11px rgba(0, 0, 0, 0.04);
-  width: ${props => props.width ? `${props.width}` : '100%'};
   display: block;
-  ${props => props.margin && `margin: ${props.margin};`}
+  margin: 16px 8px;
+  padding: 16px;
+  width: 100%;
+  min-height: 450px;
+  max-width: 280px;
+  height: auto;
   ${props => props.height && `height: ${props.height};`}
   ${props => props.onClick && `cursor: pointer;`}
   ${props => props.border && `border: ${props.border};`}
